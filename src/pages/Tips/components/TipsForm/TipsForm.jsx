83fc3a7b08157de8b2fe4f2af1dbfc690e8a64ce
nick <nikoloz.danelia@redberry.ge>
2023-05-27 22:27:32 +0400

    feat: tips page form validation

--- src/pages/Tips/components/TipsForm/TipsForm.jsx
+++ src/pages/Tips/components/TipsForm/TipsForm.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 function TipsForm() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <>
-      <form className='relative overflow-y-auto overflow-x-hidden w-full h-auto flex flex-col'>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='relative overflow-y-auto overflow-x-hidden w-full h-auto flex flex-col'
+      >
         <div className='flex flex-col gap-11'>
           <div className='mt-5'>
             <p>
@@ -23,19 +31,39 @@ function TipsForm() {
               შეხვედრები, სადაც ყველა სურვილისამებრ ჩაერთვება?*
             </label>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('onlineMeets', { required: true })}
+                value={'twice a week'}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>კვირაში ორჯერ</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('onlineMeets', { required: true })}
+                value={'once a week'}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>კვირაში ერთხელ</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('onlineMeets', { required: true })}
+                value={'once in a 2 week'}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>ორ კვირაში ერთხელ</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('onlineMeets', { required: true })}
+                value={'once a month'}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>თვეში ერთხელ</label>
             </div>
           </div>
@@ -44,27 +72,57 @@ function TipsForm() {
               კვირაში რამდენი დღე ისუვრებდი ოფისიდან მუშაობას?*
             </label>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('offlineDays', { required: true })}
+                value={0}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>0</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('offlineDays', { required: true })}
+                value={1}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>1</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('offlineDays', { required: true })}
+                value={2}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>2</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('offlineDays', { required: true })}
+                value={3}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>3</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('offlineDays', { required: true })}
+                value={4}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>4</label>
             </div>
             <div className='flex items-center gap-3'>
-              <input name='covid-test' type='radio' className='w-6 h-6' />
+              <input
+                {...register('offlineDays', { required: true })}
+                value={5}
+                type='radio'
+                className='w-6 h-6'
+              />
               <label className='text-xl'>5</label>
             </div>
           </div>
@@ -72,24 +130,32 @@ function TipsForm() {
             <label className='text-xl font-bold'>
               რას ფიქრობ ფიზიკურ შეკრებებზე?
             </label>
-            <textarea className='h-44 border border-neutral-700 p-2'></textarea>
+            <textarea
+              {...register('offlineMeetThoughts')}
+              className='h-44 border border-neutral-700 p-2'
+            ></textarea>
           </div>
           <div className='relative flex flex-col gap-5 pt-4'>
             <label className='text-xl font-bold'>
               რას ფიქრობ არსებულ გარემოზე: რა მოგწონს, რას დაამატებდი, რას
               შეცვლიდი?
             </label>
-            <textarea className='h-44 border border-neutral-700 p-2'></textarea>
+
+            <textarea
+              {...register('enviromentThoughts')}
+              className='h-44 border border-neutral-700 p-2'
+            ></textarea>
           </div>
         </div>
         <div className='flex items-center justify-between'>
           <div></div>
-          <Link
+          <button
+            type='submit'
             className='mt-7 px-7 py-3 bg-cyan-700 rounded-full text-white font-bold'
             to='/thanks'
           >
             დასრულება
-          </Link>
+          </button>
         </div>
       </form>
     </>
